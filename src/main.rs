b95a66b117ee14f0433aebca88b31c0b9bbec1e9
Daniel Mueller <deso@posteo.net>
2019-05-20 21:36:13 -0700

    Add support for listing positions
    This change adds support for listing currently held positions. To
    accomplish that we introduce a new subcommand, position, and within it
    another one, list. That is done mostly for symmetry with the structure
    of the order subcommand; right now there are no plans to add additional
    functionality to the position subcommand.
    Formatting the positions is not quite trivial as we have to ensure
    proper alignment of all values in order to make the output appealing and
    concise at the same time.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use apca::api::v1::account;
 use apca::api::v1::asset;
 use apca::api::v1::order;
 use apca::api::v1::orders;
+use apca::api::v1::positions;
 use apca::ApiInfo;
 use apca::Client;
 
@@ -47,6 +48,9 @@ enum Command {
   /// Perform various order related functions.
   #[structopt(name = "order")]
   Order(Order),
+  /// Perform various position related functions.
+  #[structopt(name = "position")]
+  Position(Position),
 }
 
 
@@ -113,6 +117,14 @@ impl FromStr for OrderId {
   fn from_str(id: &str) -> Result<Self, Self::Err> {
     Ok(OrderId(order::Id(Uuid::parse_str(id)?)))
   }
+}
+
+
+#[derive(Debug, StructOpt)]
+enum Position {
+  /// List all open positions.
+  #[structopt(name = "list")]
+  List,
 }
 
 
@@ -313,6 +325,117 @@ fn order_list(client: Client) -> Result<Box<dyn Future<Item = (), Error = String
 }
 
 
+/// The handler for the 'position' command.
+fn position(
+  client: Client,
+  position: Position,
+) -> Result<Box<dyn Future<Item = (), Error = String>>, String> {
+  match position {
+    Position::List => position_list(client),
+  }
+}
+
+
+/// Format a price value.
+///
+/// Note that this is really only the actual value without any currency.
+fn format_price(price: &Num) -> String {
+  format!("{:.2}", price)
+}
+
+
+/// Format a percentage value.
+///
+/// Note that this is really only the actual value, the percent sign is
+/// omitted because clients may need to know size of the actual value
+/// only.
+fn format_percent(percent: &Num) -> String {
+  format!("{:.2}", percent * 100)
+}
+
+
+/// List all currently open positions.
+fn position_list(client: Client) -> Result<Box<dyn Future<Item = (), Error = String>>, String> {
+  let account = client
+    .issue::<account::Get>(())
+    .map_err(|e| format!("failed to issue GET request to account endpoint: {}", e))?
+    .map_err(|e| format!("failed to retrieve account information: {}", e));
+
+  let positions = client
+    .issue::<positions::Get>(())
+    .map_err(|e| format!("failed to issue GET request to positions endpoint: {}", e))?
+    .map_err(|e| format!("failed to list positions: {}", e));
+
+  let fut = account.join(positions).and_then(|(account, mut positions)| {
+    let currency = account.currency;
+
+    positions.sort_by(|a, b| a.symbol.cmp(&b.symbol));
+
+    let qty_max = max_width(&positions, |p| format_quantity(&p.quantity).len());
+    let sym_max = max_width(&positions, |p| p.symbol.len());
+    let price_max = max_width(&positions, |p| format_price(&p.current_price).len());
+    let entry_max = max_width(&positions, |p| format_price(&p.average_entry_price).len());
+    let today_max = max_width(&positions, |p| format_price(&p.unrealized_gain_today).len());
+    let today_pct_max = max_width(&positions, |p| {
+      format_percent(&p.unrealized_gain_today_percent).len()
+    });
+    let total_max = max_width(&positions, |p| format_price(&p.unrealized_gain_total).len());
+    let total_pct_max = max_width(&positions, |p| {
+      format_percent(&p.unrealized_gain_total_percent).len()
+    });
+
+    let position_col = qty_max + 1 + sym_max + 3 + price_max + 1 + currency.len();
+    let entry = "Avg Entry";
+    let entry_col = max(entry_max + 1 + currency.len(), entry.len());
+    let today = "Today P/L";
+    let today_col = max(today_max + 1 + currency.len() + 2 + today_pct_max + 2, today.len());
+    let total = "Total P/L";
+    let total_col = max(total_max + 1 + currency.len() + 2 + total_pct_max + 2, total.len());
+
+    println!(
+      "{empty:^pos_width$} | {entry:^entry_width$} | {today:^today_width$} | {total:^total_width$}",
+      empty = "",
+      pos_width = position_col,
+      entry_width = entry_col,
+      entry = entry,
+      today_width = today_col,
+      today = today,
+      total_width = total_col,
+      total = total,
+    );
+
+    for position in &positions {
+      println!(
+        "{qty:>qty_width$} {sym:<sym_width$} @ {price:>price_width$.2} {currency} | \
+         {entry:>entry_width$} {currency} | \
+         {today:>today_width$} {currency} ({today_pct:>today_pct_width$}%) | \
+         {total:>total_width$} {currency} ({total_pct:>total_pct_width$}%)",
+        qty_width = qty_max,
+        qty = position.quantity,
+        sym_width = sym_max,
+        sym = position.symbol,
+        price_width = price_max,
+        price = position.current_price,
+        currency = currency,
+        entry_width = entry_max,
+        entry = format_price(&position.average_entry_price),
+        today_width = today_max,
+        today = format_price(&position.unrealized_gain_today),
+        today_pct_width = today_pct_max,
+        today_pct = format_percent(&position.unrealized_gain_today_percent),
+        total_width = total_max,
+        total = format_price(&position.unrealized_gain_total),
+        total_pct_width = total_pct_max,
+        total_pct = format_percent(&position.unrealized_gain_total_percent),
+      )
+    }
+    ok(())
+  });
+
+  Ok(Box::new(fut))
+}
+
+
 fn main() -> Result<(), String> {
   let opts = Opts::from_args();
   let level = match opts.verbosity {
@@ -333,6 +456,7 @@ fn main() -> Result<(), String> {
   let future = match opts.command {
     Command::Account => account(client),
     Command::Order(order) => self::order(client, order),
+    Command::Position(position) => self::position(client, position),
   }?;
 
   block_on_all(future)
