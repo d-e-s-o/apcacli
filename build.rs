8a9618cfc3b0e2b09a86730b7edf6f88d8ded666
Daniel Mueller <deso@posteo.net>
2022-02-02 07:07:27 -0800

    Adjust build.rs to error out on Git command failure
    As it turns out we missed checking the Git command that we execute
    actually succeeded, which could have caused an empty Git revision string
    to be emitted wrongly.
    With this change we add this missing check to make sure that we bubble
    up errors correctly.

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
-// Copyright (C) 2021 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2021-2022 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use std::process::Command;
 
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
 
 const GIT: &str = "git";
 
 
+/// Format a git command with the given list of arguments as a string.
 fn git_command(args: &[&str]) -> String {
   args.iter().fold(GIT.to_string() + " ", |mut cmd, arg| {
     cmd += &(" ".to_owned() + arg);
@@ -23,6 +25,20 @@ fn git(args: &[&str]) -> Result<String> {
     .args(args)
     .output()
     .with_context(|| format!("failed to run `{}`", git_command(args)))?;
+
+  if !git.status.success() {
+    let code = if let Some(code) = git.status.code() {
+      format!(" ({})", code)
+    } else {
+      String::new()
+    };
+
+    bail!(
+      "`{}` reported non-zero exit-status{}",
+      git_command(args),
+      code
+    );
+  }
 
   let output = String::from_utf8(git.stdout).with_context(|| {
     format!(
