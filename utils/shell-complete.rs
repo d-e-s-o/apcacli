4e7e96e0e43380c61ecb0c66eed164f2143f503a
Joshua Harrison <joshua@fido.investments>
2022-05-27 16:13:32 +0000

    Convert from structopt to clap v3 for argument parsing.

--- utils/shell-complete.rs
+++ utils/shell-complete.rs
@@ -5,8 +5,11 @@
 
 use std::io::stdout;
 
-use structopt::clap::Shell;
-use structopt::StructOpt;
+use clap::IntoApp;
+use clap::Parser;
+
+use clap_complete::generate;
+use clap_complete::Shell;
 
 
 #[allow(unused)]
@@ -16,19 +19,24 @@ mod apcacli {
 
 
 /// Generate a shell completion script for apcacli.
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Parser)]
 struct Args {
   /// The shell for which to generate a completion script for.
-  #[structopt(possible_values = &Shell::variants())]
+  #[clap(arg_enum)]
   shell: Shell,
   /// The command for which to generate the shell completion script.
-  #[structopt(default_value = "apcacli")]
+  #[clap(default_value = "apcacli")]
   command: String,
 }
 
 
 fn main() {
-  let args = Args::from_args();
-  let mut app = apcacli::Args::clap();
-  app.gen_completions_to(&args.command, args.shell, &mut stdout());
+  let args = Args::parse();
+  let mut app = apcacli::Args::command();
+  generate(
+    args.shell,
+    &mut app,
+    &args.command,
+    &mut stdout()
+  );
 }
