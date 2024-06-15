fa6c36c1b698d0e8226da7df361c7b720dcadd7e
Daniel Müller <deso@posteo.net>
2024-06-14 21:18:33 -0700

    Generalize shell-complete logic
    We use the same shell-complete logic for many command line applications.
    As such, we have a strong incentive to generalize it to the degree
    possible.
    This change does that, by removing all references to apcacli
    specifically and replacing them with more general alternatives.

--- utils/shell-complete.rs
+++ utils/shell-complete.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2023 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2020-2024 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #![allow(clippy::large_enum_variant, clippy::let_and_return)]
@@ -13,25 +13,25 @@ use clap_complete::Shell;
 
 
 #[allow(unused)]
-mod apcacli {
+mod prog {
   include!("../src/args.rs");
 }
 
 
-/// Generate a shell completion script for apcacli.
+/// Generate a shell completion script for the program.
 #[derive(Debug, Parser)]
 struct Args {
   /// The shell for which to generate a completion script for.
   #[clap(value_enum)]
   shell: Shell,
   /// The command for which to generate the shell completion script.
-  #[clap(default_value = "apcacli")]
+  #[clap(default_value = env!("CARGO_PKG_NAME"))]
   command: String,
 }
 
 
 fn main() {
   let args = Args::parse();
-  let mut app = apcacli::Args::command();
+  let mut app = prog::Args::command();
   generate(args.shell, &mut app, &args.command, &mut stdout());
 }
