3975ac5e19bcdde6714c90db95b830de13a3f91e
Daniel Müller <deso@posteo.net>
2025-10-12 07:57:38 -0700

    Use runtime environment variable query in build script
    It is incorrect to assume presence of the various environment variables
    used in the build script snippet at compile time [0]. Switch to using
    runtime querying instead.
    
    [0] https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts

--- build.rs
+++ build.rs
@@ -1,21 +1,23 @@
-// Copyright (C) 2021-2023 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2021-2025 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use std::env;
+
+use anyhow::Context as _;
 use anyhow::Result;
 
 use grev::git_revision_auto;
 
 
 fn main() -> Result<()> {
-  let dir = env!("CARGO_MANIFEST_DIR");
-  if let Some(git_rev) = git_revision_auto(dir)? {
-    println!(
-      "cargo:rustc-env=VERSION={} ({})",
-      env!("CARGO_PKG_VERSION"),
-      git_rev
-    );
+  let manifest_dir =
+    env::var_os("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR variable not set")?;
+  let pkg_version = env::var("CARGO_PKG_VERSION").context("CARGO_PKG_VERSION variable not set")?;
+
+  if let Some(git_rev) = git_revision_auto(manifest_dir)? {
+    println!("cargo:rustc-env=VERSION={pkg_version} ({git_rev})");
   } else {
-    println!("cargo:rustc-env=VERSION={}", env!("CARGO_PKG_VERSION"));
+    println!("cargo:rustc-env=VERSION={pkg_version}");
   }
   Ok(())
 }
