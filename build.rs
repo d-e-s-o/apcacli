eddf7d0697a92b4a6c2738d0b5f60f22139f0488
Daniel Müller <deso@posteo.net>
2026-09-19 23:13:56 -0700

    Update grev crate to 0.2
    Update the grev crate to version 0.2 to get the latest and greatest.

--- build.rs
+++ build.rs
@@ -1,23 +1,20 @@
-// Copyright (C) 2021-2025 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2021-2026 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use std::env;
 
-use anyhow::Context as _;
-use anyhow::Result;
-
-use grev::git_revision_auto;
+use grev::git_revision;
 
 
-fn main() -> Result<()> {
+fn main() {
   let manifest_dir =
-    env::var_os("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR variable not set")?;
-  let pkg_version = env::var("CARGO_PKG_VERSION").context("CARGO_PKG_VERSION variable not set")?;
+    env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR variable not set");
+  let pkg_version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION variable not set");
 
-  if let Some(git_rev) = git_revision_auto(manifest_dir)? {
+  let git_rev = git_revision(manifest_dir).expect("failed to query Git revision");
+  if let Some(git_rev) = git_rev {
     println!("cargo:rustc-env=VERSION={pkg_version} ({git_rev})");
   } else {
     println!("cargo:rustc-env=VERSION={pkg_version}");
   }
-  Ok(())
 }
